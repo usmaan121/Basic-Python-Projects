108aa15bcb26be244194b653ecd1bfafd94f98dd
usmaan121 <mrusman1.riaz@gmail.com>
2025-08-23 02:13:00 +0500

    code.py

--- Number guessing game/code.py
+++ Number guessing game/code.py
@@ -1,1 +1,15 @@
+import random
 
+guess = random.randint(1 , 100)
+
+
+while True:
+    choice = int(input("Enter the number between 1 - 100 :  "))
+    if choice < guess:
+        print("value is too low")
+    elif choice > guess:
+        print("value is too high")
+    else:
+        print("You have choose the correct number")
+        break
+        
